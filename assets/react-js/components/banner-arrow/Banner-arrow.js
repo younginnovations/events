d743491f523b20bcc1f09a2469e72bc662905deb
Nirajan <nirajanb89@gmail.com>
2017-01-06 15:36:25 +0545

    Logo url anf UI improvements

--- assets/react-js/components/banner-arrow/Banner-arrow.js
+++ assets/react-js/components/banner-arrow/Banner-arrow.js
@@ -6,12 +6,12 @@ const arrowScroll = () => {
         $('a.arrow-wrap[href*=#]:not([href=#]),a.arrow-past-wrap[href*=#]:not([href=#])').on('click',function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash),
-                    headerHeight = $('.section-banner').height() - 5;
+                    headerHeight = $('.section-banner').height() - 15;
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html,body').animate({
                         scrollTop: target.offset().top - headerHeight
-                    }, 1000);
+                    }, 500);
                     return false;
                 }
             }
--- assets/react-js/components/past-event-header/PastEventHeader.js
+++ assets/react-js/components/past-event-header/PastEventHeader.js
@@ -2,32 +2,13 @@ import React from 'react';
 
 const pastEventScroll = () => {
     $(function () {
-        // $(".arrow-past-wrap").click(function() {
-        //     $(".past-event-header").animate(
-        //         {
-        //             top: "40"
-        //         }, 1000).queue(function(){
-        //         $(".section-banner").addClass('active');
-        //
-        //     });
-        // });
-
         var sectionSliderTop = $(".section-slider").position().top;
-
         $(window).on('scroll',function() {
             if ($(window).scrollTop() < sectionSliderTop) {
-                // $(".past-event-header").stop().animate(
-                //         {
-                //             top: "auto"
-                //         }, 1000);
                 $(".past-event-header").stop();
                 $(".section-banner").removeClass('active');
             }
             else {
-                // $(".past-event-header").stop().animate(
-                //     {
-                //         top: "40"
-                //     }, 1000);
                 $(".past-event-header").stop();
                 $(".section-banner").addClass('active');
             }
